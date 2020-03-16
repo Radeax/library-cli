b6d0f5db5651acea7be4296e93c9ce15d8d04610
jbaker425 <jamesbaker425@gmail.com>
2020-03-16 00:05:47 -0400

    Update project.py (#9)
    ;)

--- project.py
+++ project.py
@@ -1,35 +1,32 @@
-# We can put our names up here
-# Started on 3/11/20
-# Runs the program
+#We can put our names up here
+#Started on 3/11/20
+#Runs the program
 import librarian as lib
 import borrower as bor
+import borrower1 as bor1
+import admin as ad
 import check
+from librarysql import *
 
-
-def main():
-    inp = 0
-    print("\nWelcome to the GCIT Library Management System.")
-
-    # Main menu loop
-    while inp != 4:
-        print("\nWhich category of a user are you?\n\n1) Librarian\n2) Administrator\n3) Borrower\n4) To Quit\n")
-        inp = input("Please enter a number between 1 and 4: ")
-        # Check for valid input, then proceed
-        if check.validInput(inp, 1, 4):
-            inp = int(inp)
-            if inp == 1:
-                lib.runLibrarian()
-            elif inp == 2:
-                runAdministrator()
-            elif inp == 3:
-                bor.runBorrower()
-            # If inp = 4
-            else:
-                print("\nGoodbye!")
-
-
-def runAdministrator():
-    print("\nYour input was 2, You are an Administrator")
-
+def main():	
+	initialData()
+	print ("\nWelcome to the GCIT Library Management System.")
+	inp = 0
+	#Main menu loop
+	while inp != 4:
+		print ("\nWhich category of a user are you?:\n\n1) Librarian\n2) Administrator\n3) Borrower\n4) To Quit")
+		inp = input("\nPlease enter a number between 1 and 4: ")
+		#Check for valid input, then proceed
+		if check.validInput(inp, 1, 4):
+			inp = int(inp)
+			if inp == 1:
+				lib.runLibrarian()
+			elif inp == 2:
+				ad.runAdministrator()
+			elif inp == 3:
+				bor1.runBorrower()
+			#If inp = 4
+			else:
+				print ("\nGoodbye!")
 
 main()
